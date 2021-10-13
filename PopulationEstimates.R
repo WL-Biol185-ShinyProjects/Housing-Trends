ae928ead531073f2e45ab8f13c8a65bd8f1f122a
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-10-13 16:05:08 -0400

    cleaning pop data, unsuccessful

--- PopulationEstimates.R
+++ PopulationEstimates.R
@@ -1,4 +1,11 @@
 library(readxl)
 library(ggplot2)
+library(tidyverse)
 
+tidy_pop_data <- read_excel("~/Housing-Trends/co-est2019-annres-51 (2).xlsx", 
+                           skip = 3)
 View(tidy_pop_data)
+
+yearly_pop_est <- gather(tidy_pop_data[4:13], key = "year", value = "index")
+View(yearly_pop_est)
+
