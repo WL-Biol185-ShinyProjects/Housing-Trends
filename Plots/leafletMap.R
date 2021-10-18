051d5bee208c374446f4ed035d8fb1cd1405b33c
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-10-18 15:50:12 -0400

    - Main navigation - Leaflet map initialization

---
+++ Plots/leafletMap.R
@@ -0,0 +1,3 @@
+library(leaflet)
+
+leafletMap <- leaflet() %>% setView(lng = -79.442778, lat = 37.783889, zoom = 12) %>% addTiles()
--- ui.R
+++ ui.R
@@ -1,13 +1,25 @@
 library(shiny)
+library(leaflet)
 
-fluidPage(
-  # The page title
-  titlePanel("Race across Time in VA"),
-  
-  # A row for this plot that has a sidebar
-  fluidRow(
-    column(9,
-        plotOutput("racePlot")
-        )
-  )
-)
+navbarPage("Housing Trends",
+           tabPanel("Race Across Time in VA", 
+                    fluidPage(
+                      # The page title
+                      titlePanel("Race across Time in VA"),
+                      
+                      # A row for this plot that has a sidebar
+                      fluidRow(
+                        column(9,
+                               plotOutput("racePlot")
+                        )
+                      )
+                    )
+                  ),
+           # Page for our map
+           tabPanel("Map", leafletOutput("theMap", height = 1000)),
+           
+           # Page for something else in the future
+           tabPanel("Another Page",
+                    fluidRow("This is another page")
+                    )
+          )
