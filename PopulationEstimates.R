c1f1aa1b47dfe40c3e76a6e85f1d9ca758f82f33
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-10-18 15:50:37 -0400

    Updated Pop Data

--- PopulationEstimates.R
+++ PopulationEstimates.R
@@ -6,6 +6,11 @@ tidy_pop_data <- read_excel("~/Housing-Trends/co-est2019-annres-51 (2).xlsx",
                            skip = 3)
 View(tidy_pop_data)
 
-yearly_pop_est <- gather(tidy_pop_data[4:13], key = "year", value = "index")
-View(yearly_pop_est)
+tidy_pop_data$Census <- NULL
+tidy_pop_data$`Estimates Base`<-NULL
+tidy_pop_data <- tidy_pop_data[-c(1,135:140),] 
+colnames(tidy_pop_data)[1] <- "county"
+
+yearly_pop_est <- gather(tidy_pop_data[1:11], key = "year", value = "index")
+
 
