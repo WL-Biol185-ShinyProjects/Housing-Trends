c46b5eccfabf56b55b2e2ec2758036f98b9e7c71
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-10-24 12:10:55 -0400

    Fixed Data Table for yearly_pop_est

--- PopulationEstimates.R
+++ PopulationEstimates.R
@@ -1,12 +1,13 @@
 library(readxl)
 library(ggplot2)
 library(tidyverse)
-
+library(shiny)
+library(sf)
 library(leaflet)
 library(rgdal)
 
 #importing population data
-tidy_pop_data <- read_excel("~/Housing-Trends/co-est2019-annres-51 (2).xlsx", 
+tidy_pop_data <- read_excel("~/Housing-Trends/Data/Population Estimates/co-est2019-annres-51 (2).xlsx", 
                            skip = 3)
 View(tidy_pop_data)
 
@@ -16,9 +17,19 @@ tidy_pop_data$`Estimates Base`<-NULL
 tidy_pop_data <- tidy_pop_data[-c(1,135:140),] 
 colnames(tidy_pop_data)[1] <- "county"
 
-yearly_pop_est <- gather(tidy_pop_data[1:11], key = "year", value = "index")
+#gathering my data
+yearly_pop_est <- gather(tidy_pop_data, key = "year", value = "pop_estimate", `2010`:`2019`)
+View(yearly_pop_est)
 
-#Attempts to create map
+
+
+ggplot(yearly_pop_est, aes(pop_estimate, year, color = pop_estimate)) + geom_point()
+ggplot(yearly_pop_est, aes(county, estimate)) + geom_point()
+ggplot(yearly_pop_est, aes(county, year, color = pop_estimate)) + geom_point()
+
+#attempts to create map
+#left join data structure on left and our data on the right// use by argument, reassign back to geo structure data
+#-- THEN do leaflet to fill the polygons
 
 geo <- readOGR("Data/counties.geo.json")
 
@@ -30,3 +41,45 @@ geo@data <- newData
 leaflet(geo) %>% 
   addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
               opacity = 1.0, fillOpacity = 0.5)
+
+#UI attempt?
+shape <- tigris::counties(state = "VA", class = "sf")
+
+# Define UI 
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Test 1-2-3"),
+  
+  # Top panel with county name
+  verticalLayout(
+    
+    wellPanel(textOutput("cnty")),
+    
+    # the map itself
+    mainPanel(
+      leafletOutput("map")
+    )
+  )
+)
+
+
+# Define server logic       
+server <- function(input, output) {
+  
+  output$map <- renderLeaflet({
+    leaflet() %>% 
+      addProviderTiles("Stamen.Toner") %>% 
+      addPolygons(data = shape, 
+                  fillColor = "aliceblue", 
+                  color = "grey",
+                  layerId = ~COUNTYNS)
+  })
+  
+  # this is the fun part!
+  observe({ 
+    event <- input$map_shape_click
+    output$cnty <- renderText(shape$NAMELSAD[shape$COUNTYNS == event$id])
+    
+  })
+}
