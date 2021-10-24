5ce27278d428503690813b9e2763032f50ab3fdd
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-10-24 12:44:47 -0400

    Got something on the map

--- Plots/leafletMap.R
+++ Plots/leafletMap.R
@@ -11,12 +11,12 @@ newData  <- left_join(geo@data, clean_dcohu, by = c("NAME" = "county",
 geo@data <- newData
 
 bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
-pal <- colorBin("YlOrRd", domain = geo@data$units, bins = bins)
+pal <- colorBin("YlOrRd", domain = geo$units, bins = bins)
 
 
 leaflet(geo) %>% 
   setView(-103.483330, 38.712046, 4) %>% # Just showing US for now
-  addPolygons(fillColor = ~pal(geo@data$units),color = "#444444", weight = 1, smoothFactor = 0.5,
+  addPolygons(fillColor = ~pal(geo$units),color = "#444444", weight = 1, smoothFactor = 0.5,
               opacity = 1.0, fillOpacity = 0.5)
 
 leafletMap <- leaflet(geo) %>% 
