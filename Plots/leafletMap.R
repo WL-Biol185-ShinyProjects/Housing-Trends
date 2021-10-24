a3ac70cc5006548bc69935b8780aaefb87c96d30
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-10-24 13:06:28 -0400

    working now

--- Plots/leafletMap.R
+++ Plots/leafletMap.R
@@ -6,18 +6,28 @@ source("DCOHU_data_cleanup.R")
 
 geo <- readOGR("Data/counties.json")
 
-newData  <- left_join(geo@data, clean_dcohu, by = c("NAME" = "county", 
+# Filtering by year to show on map
+# We should replace this with input from something like a dropdown with the years in the decade to choose from
+to_join <- clean_dcohu %>%
+  filter(year == 2010)
+
+newData  <- left_join(geo@data, to_join, by = c("NAME" = "county", 
                                                     "LSAD" = "LSAD"))
 geo@data <- newData
 
+# Prepping colors for chloropleth
 bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
 pal <- colorBin("YlOrRd", domain = geo$units, bins = bins)
 
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g units",
+  geo$NAME, geo$units
+) %>% lapply(htmltools::HTML)
 
 leaflet(geo) %>% 
   setView(-103.483330, 38.712046, 4) %>% # Just showing US for now
-  addPolygons(fillColor = ~pal(geo$units),color = "#444444", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.5)
+  addPolygons(fillColor = ~pal(geo$units),color = "white", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5, label = labels)
 
 leafletMap <- leaflet(geo) %>% 
   setView(lng = -79.442778, lat = 37.783889, zoom = 12) %>% 
