86b7397a2a05454dfc16f3d7d9cbc8e53bd84d13
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-10-27 16:13:21 -0400

    Removed trailing commas in csvs

--- Race_Demographics_cleanup.R
+++ Race_Demographics_cleanup.R
@@ -3,7 +3,7 @@ library(readr)
 library(data.table)
 library(tidyverse)
 
-df <- list.files(path="Data/Census Race Estimates for VA", full.names = TRUE) %>% 
-  lapply(read_csv) %>% 
-  bind_rows
-
+all_data <- list.files(path = "Data/Census Race Estimates for VA",
+                       pattern = "*.csv", full.names = TRUE) %>% 
+  lapply(read_csv) %>%                                      
+  bind_rows      
