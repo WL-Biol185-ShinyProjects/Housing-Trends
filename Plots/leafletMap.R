f1deed6569647989451122f15bac5ce5cadfc958
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-11-01 15:47:43 -0400

    DCOHU map working with slider

--- Plots/leafletMap.R
+++ Plots/leafletMap.R
@@ -4,42 +4,46 @@ library(tidyverse)
 
 source("Data_Prep/DCOHU_data_cleanup.R")
 
-geo <- readOGR("Data/counties.json")
+leafletMap <- function (originalGeo, yearToUse) {
+  # Reset json
+  geo <- originalGeo
+  
+  # Filtering by year to show on map
+  # We should replace this with input from something like a dropdown with the years in the decade to choose from
+  to_join <- clean_dcohu %>%
+    filter(year == yearToUse)
+  
+  # Left join our data into the geo json's data
+  newData  <- left_join(geo@data, to_join, by = c("NAME" = "county", 
+                                                  "LSAD" = "LSAD"))
+  
+  # Replace with the joined version
+  geo@data <- newData
+  
+  # Removing the polygons of non-VA states
+  geo@polygons[which(geo@data$STATE != 51)] <- NULL
+  
+  # Removing non-VA data
+  geo@data <- geo@data[geo@data$STATE == 51,]
+  
+  # Prepping colors for chloropleth
+  bins <- c(0,20000,30000,40000, 50000, Inf)
+  pal <- colorBin("YlOrBr", domain = geo$units, na.color = "transparent", bins = bins)
+  
+  # Labels to show on hover
+  labels <- sprintf(
+    "<strong>%s</strong><br/>%g units",
+    geo$NAME, geo$units
+  ) %>% lapply(htmltools::HTML)
+  
+  
+  # Final leaflet map to output
+  leaflet(geo) %>% 
+    setView(-79.442778, 37.783889, 8) %>%
+    addPolygons(fillColor = ~pal(geo$units),color = "white", weight = 1, smoothFactor = 0.5,
+                opacity = 1.0, fillOpacity = 0.5, label = labels) %>%
+    addLegend( pal=pal, values=geo$units, opacity=0.9, title = "Number of Units", position = "bottomleft" )
+}
 
-# Filtering by year to show on map
-# We should replace this with input from something like a dropdown with the years in the decade to choose from
-to_join <- clean_dcohu %>%
-  filter(year == 2010)
-
-# Left join our data into the geo json's data
-newData  <- left_join(geo@data, to_join, by = c("NAME" = "county", 
-                                                "LSAD" = "LSAD", 
-                                                "STATE" = "state"))
-
-# Replace with the joined version
-geo@data <- newData
-
-# Removing the polygons of non-VA states
-geo@polygons[which(geo@data$STATE != 51)] <- NULL
-
-# Removing non-VA data
-geo@data <- geo@data[geo@data$STATE == 51,]
-
-
-
-# Prepping colors for chloropleth
-bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
-pal <- colorBin("YlOrRd", domain = geo$units, bins = bins)
-
-# Labels to show on hover
-labels <- sprintf(
-  "<strong>%s</strong><br/>%g units",
-  geo$NAME, geo$units
-) %>% lapply(htmltools::HTML)
-
-
-# Final leaflet map to output
-leafletMap <- leaflet(geo) %>% 
-  setView(-79.442778, 37.783889, 8) %>% # Just showing US for now
-  addPolygons(fillColor = ~pal(geo$units),color = "white", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.5, label = labels)
+# geo <- readOGR("Data/counties.json")
+# leafletMap(geo, 2015)
