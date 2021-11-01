9b6cfe3831d812e9ff68f2f8b929aaeac8b346fa
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-11-01 16:18:44 -0400

    Data ready to be placed on geomap

--- CrimeEstimates.R
+++ CrimeEstimates.R
@@ -9,6 +9,8 @@ crime2019 <- read_csv("Data/Crime/R12939511_SL050.csv",
                                              `Qualifying Name` = col_skip(), State = col_skip(), 
                                              `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                              `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+  crime2019 <- crime2019[-c(1),] 
+  colnames(crime2019)[2] <- "2019"
 View(crime2019)
 
 crime2018 <- read_csv("Data/Crime/R12942816_SL050.csv", 
@@ -17,6 +19,8 @@ crime2018 <- read_csv("Data/Crime/R12942816_SL050.csv",
                                        `Qualifying Name` = col_skip(), State = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+  crime2018 <- crime2018[-c(1),] 
+  colnames(crime2018)[2] <- "2018"
 View(crime2018)
 
 crime2017 <- read_csv("Data/Crime/R12942832_SL050.csv", 
@@ -25,6 +29,8 @@ crime2017 <- read_csv("Data/Crime/R12942832_SL050.csv",
                                        `Qualifying Name` = col_skip(), State = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2017 <- crime2017[-c(1),] 
+  colnames(crime2017)[2] <- "2017"
 View(crime2017)
 
 crime2016 <- read_csv("Data/Crime/R12942840_SL050.csv", 
@@ -33,6 +39,8 @@ crime2016 <- read_csv("Data/Crime/R12942840_SL050.csv",
                                        `Qualifying Name` = col_skip(), State = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2016 <- crime2016[-c(1),] 
+colnames(crime2016)[2] <- "2016"
 View(crime2016)
 
 crime2015 <- read_csv("Data/Crime/R12942863_SL050.csv", 
@@ -41,6 +49,8 @@ crime2015 <- read_csv("Data/Crime/R12942863_SL050.csv",
                                        `Qualifying Name` = col_skip(), State = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2015 <- crime2015[-c(1),] 
+colnames(crime2015)[2] <- "2015"
 View(crime2015)
 
 crime2014 <- read_csv("Data/Crime/R12942871_SL050.csv", 
@@ -48,6 +58,8 @@ crime2014 <- read_csv("Data/Crime/R12942871_SL050.csv",
                                        FIPS = col_skip(), `Qualifying Name` = col_skip(), 
                                        State = col_skip(), `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2014 <- crime2014[-c(1),] 
+colnames(crime2014)[2] <- "2014"
 View(crime2014)
 
 
@@ -56,6 +68,8 @@ crime2013 <- read_csv("Data/Crime/R12942877_SL050.csv",
                                        FIPS = col_skip(), `Qualifying Name` = col_skip(), 
                                        State = col_skip(), `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2013 <- crime2013[-c(1),] 
+colnames(crime2013)[2] <- "2013"
 View(crime2013)
 
 crime2012 <- read_csv("Data/Crime/R12942884_SL050.csv", 
@@ -63,6 +77,8 @@ crime2012 <- read_csv("Data/Crime/R12942884_SL050.csv",
                                        FIPS = col_skip(), `Qualifying Name` = col_skip(), 
                                        State = col_skip(), `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2012 <- crime2012[-c(1),] 
+colnames(crime2012)[2] <- "2012"
 View(crime2012)
 
 crime2011 <- read_csv("Data/Crime/R12942889_SL050.csv", 
@@ -70,6 +86,8 @@ crime2011 <- read_csv("Data/Crime/R12942889_SL050.csv",
                                        FIPS = col_skip(), `Qualifying Name` = col_skip(), 
                                        State = col_skip(), `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                        `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2011 <- crime2011[-c(1),] 
+colnames(crime2011)[2] <- "2011"
 View(crime2011)
 
 crime2010<- read_csv("Data/Crime/R12942896_SL050.csv", 
@@ -77,12 +95,30 @@ crime2010<- read_csv("Data/Crime/R12942896_SL050.csv",
                                       FIPS = col_skip(), `Qualifying Name` = col_skip(), 
                                       State = col_skip(), `Total Violent and Property Crimes Rate: Property Crimes Rate` = col_skip(), 
                                       `Total Violent and Property Crimes Rate: Violent Crimes Rate` = col_skip()))
+crime2010 <- crime2010[-c(1),] 
+colnames(crime2010)[2] <- "2010"
 View(crime2010)
 
 
-datatest<-  left_join(crime2010, crime2011, by='Name of Area') %>%
-  left_join(., crime2012, by='Name of Area') 
-View(datatest)
+crime_est<-left_join(crime2019, crime2018, by='Name of Area') %>%
+  left_join(.,crime2017, by= 'Name of Area') %>%
+  left_join(.,crime2016, by= 'Name of Area') %>%
+  left_join(.,crime2015, by= 'Name of Area') %>%
+  left_join(.,crime2014, by= 'Name of Area') %>%
+  left_join(.,crime2013, by= 'Name of Area') %>%
+  left_join(.,crime2012, by= 'Name of Area') %>%
+  left_join(.,crime2011, by= 'Name of Area') %>%
+  left_join(.,crime2010, by= 'Name of Area') 
+View(crime_est)
+
+yearly_crime_est <- gather(crime_est, key = "year", value = "total_violent_pop_crime_estimate", `2010`:`2019`)
+yearly_crime_est$LSAD <- word(yearly_crime_est$`Name of Area`, -1)
+yearly_crime_est$county <- gsub("\\s*\\w*$", "", yearly_crime_est$`Name of Area`)
+yearly_crime_est <- col_skip(`Name of Area`)
+yearly_crime_est$`Name of Area` <- NULL
+View(yearly_crime_est)
+
+
 
 
 
