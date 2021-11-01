df9a8ddd7259dcabacb9d282216337ffbca7bd6f
Betelihim <haileb22@mail.wlu.edu>
2021-11-01 14:05:53 -0400

    I cleaned up the race demographics datasets and merged them so I can left join them on the leaflet map of VA

--- Race_Demographics_cleanup.R
+++ Race_Demographics_cleanup.R
@@ -1,9 +1,27 @@
-library(dplyr)
-library(readr)
-library(data.table)
 library(tidyverse)
+library(readr)
+library(dplyr)
 
-all_data <- list.files(path = "Data/Census Race Estimates for VA",
-                       pattern = "*.csv", full.names = TRUE) %>% 
-  lapply(read_csv) %>%                                      
-  bind_rows      
+Census_2012_RaceEstimates_forVA <- read_csv("Data/Census Race Estimates for VA/Census_2012_RaceEstimates_forVA.csv")
+
+# remove rows where all values are NA:
+Census_2012_RaceEstimates_forVA %>% filter_all(any_vars(!is.na(.)))
+Census_2012_RaceEstimates_forVA %>% filter_all(any_vars(complete.cases(.)))
+View(Census_2012_RaceEstimates_forVA)
+
+Census_2014_RaceEstimates_forVA <- read_csv("Data/Census Race Estimates for VA/Census_2014_RaceEstimates_forVA.csv")
+
+#remove columns where all values are NA
+Census_2014_RaceEstimates_forVA= select(Census_2014_RaceEstimates_forVA, -8)
+View(Census_2014_RaceEstimates_forVA)
+
+merged_race_data <- rbind("Census_2011_RaceEstimates_forVA,
+               Census_2012_RaceEstimates_forVA,
+               Census_2013_RaceEstimates_forVA,
+               Census_2014_RaceEstimates_forVA,
+               Census_2015_RaceEstimates_forVA,
+               Census_2016_RaceEstimates_forVA,
+               Census_2017_RaceEstimates_forVA,
+               Census_2018_RaceEstimates_forVA,
+               Census_2019_RaceEstimates_forVA_0")
+
