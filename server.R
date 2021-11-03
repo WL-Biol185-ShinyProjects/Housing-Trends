725878fd7aebcc6968f0d54062144a20b7eb8789
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-11-03 14:54:05 -0400

    Population estimates map now on web app

--- server.R
+++ server.R
@@ -4,7 +4,8 @@ library(leaflet)
 
 # Importing the plots here so we can separate each plot's logic and prevent conflict
 source("Plots/racePlot.R")
-source("Plots/leafletMap.R")
+source("Plots/housingLeafletMap.R")
+source("Plots/popEstLeafletMap.R")
 
 # Importing county json only once to pass in where needed
 geo <- readOGR("Data/counties.json")
@@ -17,5 +18,10 @@ geo <- readOGR("Data/counties.json")
 function (input, output) {
   # Rendering the bar plot
   output$racePlot <- renderPlot({racePlot})
-  output$theMap <- renderLeaflet({leafletMap(geo, input$year)})
+  
+  # Rendering the occupied housing units map
+  output$housingMap <- renderLeaflet({housingLeafletMap(geo, input$housingYear)})
+  
+  # Rendering the population estimates map
+  output$popEstMap <- renderLeaflet({popEstLeafletMap(geo, input$popEstYear)})
 }
