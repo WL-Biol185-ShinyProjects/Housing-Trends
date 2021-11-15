b0b0553b2abdb68e81f0bdb817388afbbecbaf56
Betelihim <haileb22@mail.wlu.edu>
2021-11-15 16:09:50 -0500

    created a line graph for the income per capita of virginia as a whole from 2010-2019

---
+++ IncomePerCapita.R
@@ -0,0 +1,9 @@
+#Per capita income in the past 12 months from 2010-2019 in VA Plot
+#Creating a line graph
+library(ggplot2)
+library(tidyverse)
+Income_Per_Capita <- read_csv("Data/incomepercapitainVA.csv")
+  ggplot(Income_Per_Capita, aes(x = year , y = per.capita.income))+ geom_line(stat='identity')
+
+
+  
