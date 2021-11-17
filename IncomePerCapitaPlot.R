e50a537bc3762e8aa6f10f448d548151c6ff06e9
Betelihim <haileb22@mail.wlu.edu>
2021-11-17 15:13:06 -0500

    deleted a copy of the incomepercapita file and also amended the original file

--- IncomePerCapitaPlot.R
+++
@@ -1,9 +0,0 @@
-#Per capita income in the past 12 months from 2010-2019 in VA Plot
-#Creating a line graph
-library(ggplot2)
-library(tidyverse)
-Income_Per_Capita <- read_csv("Data/incomepercapitainVA.csv")
-  ggplot(Income_Per_Capita, aes(x = year , y = per.capita.income))+ geom_line(stat='identity')
-
-
-  
