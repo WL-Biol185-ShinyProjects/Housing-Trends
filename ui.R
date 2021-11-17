48b8e7da890565d9670da4e9bc235872b58b086c
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-11-17 16:05:19 -0500

    Removed nested fluidRow

--- ui.R
+++ ui.R
@@ -39,18 +39,17 @@ dashboardPage(
               ),
       # Bar plot of Race Across Time
       tabItem(tabName="race_in_va",
-              h2("Race Across Time in Virginia"),
               fluidRow(
+                h2("Race Across Time in Virginia"),
                 box(status = "primary", width = 12, plotOutput("racePlot")),
                 h2("Race Across Time per County"),
-                fluidRow(
-                  box(status = "primary", width = 8, plotOutput("racePlotII")),
-                  box(
-                    title = "Controls",
-                    status = "warning",
-                    width = 4,
-                    selectInput("county", "County", choices = unique(merged_race_data$county))
-                  )
+                box(status = "primary", width = 8, plotOutput("racePlotII")),
+                box(
+                  title = "Controls",
+                  status = "warning",
+                  width = 4,
+                  selectInput("county", "County", choices = unique(merged_race_data$county))
+          
                 )
               )
       )
