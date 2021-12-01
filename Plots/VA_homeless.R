67aaa4fbfd1dad6deafebc0e0cc60899cafd98da
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-12-01 14:16:54 -0500

    General VA PIT Count of Homelessness

---
+++ Plots/VA_homeless.R
@@ -0,0 +1,7 @@
+library(readxl)
+
+X2010_2015_PIT_Counts_by_State <- read_excel("Data/Homelessness/2010_2015_PIT_Counts_by_State.xlsx")
+
+X2010_2015_PIT_Counts_by_State %>% ggplot(aes(X2010_2015_PIT_Counts_by_State$Year, X2010_2015_PIT_Counts_by_State$Total_Homeless)) + geom_line() + labs(title="Homelessness in Virginia", subtitle="(Point in Time 2010-2015)",
+                                                                                                                                                        x ="Year", y = "Counts", caption  = "2015 Annual Homeless Assessment Report (AHAR)")
+
