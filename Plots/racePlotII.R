428d08e9cedd5730d732f94da83dd9eb7a6dd33d
Carolina Rubio Regalado <rubioregaladoc22@mail.wlu.edu>
2021-12-06 15:48:52 -0500

    Variables and Titles (oh my)

--- Plots/racePlotII.R
+++ Plots/racePlotII.R
@@ -6,5 +6,5 @@ library(tidyverse)
 
 racePlotII <- function (countyName) {
   merged_race_data %>% mutate(Year = factor(Year)) %>% filter(county == countyName, LSAD == "County") %>%
-    group_by(Race, Year)%>% ggplot(aes(Year, Race_estimate, color= Race, group=Race)) + geom_line() + scale_y_continuous(trans='log10')
+    group_by(Race, Year)%>% ggplot(aes(Year, Race_estimate, color= Race, group=Race)) + geom_line() + scale_y_continuous(trans='log10') + labs (y = "Race Estimates (Counts)", caption= "Source: US Census") 
 }
