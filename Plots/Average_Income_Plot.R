1f09e581b0369aa5197660c8afeccbcdf067372f
Betelihim <haileb22@mail.wlu.edu>
2021-12-08 13:04:51 -0500

    fixed axis titles and listed the source under the graphs

--- Plots/Average_Income_Plot.R
+++ Plots/Average_Income_Plot.R
@@ -1,2 +1,3 @@
 Average_Income <- merged_income_race_data %>% mutate(Year = factor(Year)) %>% 
-  ggplot(aes(x = Year, y = Mean_Income, color = Race_Ethnicity, group = Race_Ethnicity)) + geom_line()
+  ggplot(aes(x = Year, y = Mean_Income, color = Race_Ethnicity, group = Race_Ethnicity)) + geom_line() + 
+  labs(x ="Year", y = "Average Income", caption  = "Source: US Census Bureau")
--- Plots/IncomePerCapita.R
+++ Plots/IncomePerCapita.R
@@ -4,7 +4,8 @@ Income_Per_Capita <- read_csv("Data/incomepercapitainVA.csv")
  
 #Make the graph
 Income_Per_Capita_Plot <- Income_Per_Capita %>% mutate(year = factor(year)) %>% group_by(per.capita.income, year) %>% 
-    ggplot(aes(x = year, y = per.capita.income, group = 1)) + geom_line(stat='identity')
+    ggplot(aes(x = year, y = per.capita.income, group = 1)) + geom_line(stat='identity') + labs(x ="Year", y = "Income Per Capita", caption  = "Source: US Census Bureau")
+
 
 # function to get income per capita by year for info box on main map
 perCapitaByYear <- function (yearToUse) {
