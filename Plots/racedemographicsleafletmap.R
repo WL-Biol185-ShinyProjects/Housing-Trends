dcb793490a88c8002ae6623300a14759945fda92
Betelihim <haileb22@mail.wlu.edu>
2021-12-08 00:40:34 -0500

    deleted this file bc Carolina made it a line graph instead of a map

--- Plots/racedemographicsleafletmap.R
+++
@@ -1,7 +0,0 @@
-library(leaflet)
-library(rgdal)
-library(tidyverse)
-
-#The race data needs its own separate map because it requires an additional drop down menu for the quantities of people in each race category
-source("Data_Prep/Race_Demographics_cleanup.R")
-
